920a46a3b1b9bdc9f1b33dc867ed2dafb8206541
Troy Ramsey <troypramsey@gmail.com>
2020-10-09 14:48:49 -0700

    added sizeref to bubble chart markers

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,7 @@ d3.json('/static/js/samples.json').then((sample) => {
         // Appending select object
         var dropdown = d3.select('#dropdown-container')
             .append('select')
+            .classed('form-control form-control-lg', true)
 
         // Appending select options
         var selections = dropdown.selectAll('option')
@@ -16,34 +17,43 @@ d3.json('/static/js/samples.json').then((sample) => {
 
         // INITIAL PAGE LOAD
         // Get value from select input
-        var graph_data = sample.samples
+        var graphData = sample.samples
         var cardData = sample.metadata
 
         // Defining INIT() function
         function init() {
             
             // Bar graph data declaration
-            let data = [{
-                x: graph_data[0].sample_values.sort((a, b) => (b - a)).slice(0, 10).reverse(),
-                y: graph_data[0].otu_ids.map(d => `OTU ${d}`),
-                text: graph_data[0].otu_labels,
+            let sortedData = graphData[0].sample_values.sort((a, b) => (b - a)).slice(0, 10).reverse()
+
+            let barData = [{
+                x: sortedData,
+                y: graphData[0].otu_ids.map(d => `OTU ${d}`).slice(0, 10).reverse(),
+                text: graphData[0].otu_labels,
                 type: 'bar',
                 orientation: 'h'
             }]
 
             // Bubble plot data declaration
             let bubbleData = [{
-                x: graph_data[0].otu_ids.map(d => `OTU ${d}`),
-                y: graph_data[0].sample_values,
+                x: graphData[0].otu_ids.map(d => `OTU ${d}`),
+                y: graphData[0].sample_values,
                 marker: {
-                    size: graph_data[0].sample_values
+                    size: graphData[0].sample_values,
+                    color: graphData[0].otu_ids,
+                    sizeref: 1.5
                 },
-                mode: 'markers'
+                mode: 'markers',
+                text: graphData[0].otu_labels
             }]
+
+            let bubbleLayout = {
+                height: 800,
+            }
             
             // Initial plots
-            Plotly.newPlot('plot', data)
-            Plotly.newPlot('bubble-plot', bubbleData)
+            Plotly.newPlot('plot', barData)
+            Plotly.newPlot('bubble-plot', bubbleData, bubbleLayout)
 
             // Initial card
             let card = d3.select('#card-body')
@@ -51,6 +61,7 @@ d3.json('/static/js/samples.json').then((sample) => {
                 .data(Object.entries(cardData[0]))
                 .join('li')
                 .text(d => d[0] + ': ' + d[1])
+                .classed('list-group-item', true)
 
 
         }
@@ -61,25 +72,33 @@ d3.json('/static/js/samples.json').then((sample) => {
 
         // Event handler to update charts
         function updatePlotly() {
+            
             let id = dropdown.node().value
-            let newId = graph_data.filter(d => d.id === id)
+            let chartId = graphData.filter(d => d.id === id)
             let cardId = cardData.filter(d => d.id.toString() === id)
 
             // H-Bar update
             let x = [], y = []
+            let sortedData = chartId[0].sample_values.sort((a, b) => (b - a)).slice(0, 10).reverse()
 
-            x = newId[0].sample_values.sort((a, b) => (b - a)).slice(0, 10).reverse()
-            y = newId[0].otu_ids.map(d => `OTU ${d}`)
+            x = sortedData
+            y = chartId[0].otu_ids.map(d => `OTU ${d}`).slice(0, 10).reverse()
 
             Plotly.restyle('plot', 'x', [x])
             Plotly.restyle('plot', 'y', [y])
 
             // Bubble chart update
-            x = newId[0].otu_ids.map(d => `OTU ${d}`)
-            y = newId[0].sample_values
+            x = chartId[0].otu_ids.map(d => `OTU ${d}`)
+            y = chartId[0].sample_values
+            let markerSize = chartId[0].sample_values
+            let markerColor = chartId[0].otu_ids
+            let text = chartId[0].otu_labels
 
             Plotly.restyle('bubble-plot', 'x', [x])
             Plotly.restyle('bubble-plot', 'y', [y])
+            Plotly.restyle('bubble-plot', 'marker.size', [markerSize])
+            Plotly.restyle('bubble-plot', 'marker.color', [markerColor])
+            Plotly.restyle('bubble-plot', 'text', [text])
 
             // Card update
             card = d3.select('#card-body')
@@ -87,6 +106,7 @@ d3.json('/static/js/samples.json').then((sample) => {
                 .data(Object.entries(cardId[0]))
                 .join('li')
                 .text(d => d[0] + ': ' + d[1])
+                .classed('list-group-item', true)
             }
 
 
